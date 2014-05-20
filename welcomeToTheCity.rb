b2d6858e4f7fb87816983c5c9858c6c9fb1054b1
Stacy <codecalypso@users.noreply.github.com>
2014-05-19 23:48:34 -0500

    Create welcomeToTheCity.rb

---
+++ welcomeToTheCity.rb
@@ -0,0 +1,15 @@
+
+#Create a method say_hello that takes as input a name, city, and state to welcome a person.
+#Note that name will be an array consisting of one or more values that should be joined together 
+#with one space betweeen each,and the length of the name array in test cases will vary.
+
+
+
+
+def say_hello(name=[], city, state)
+  return "Hello" + ", " + name[0..-1].join(" ") + "! " +
+  "Welcome to " + city + ", " + state + "!"
+   
+end
+
+say_hello(['John', 'Smith'], 'Phoenix', 'Arizona')
